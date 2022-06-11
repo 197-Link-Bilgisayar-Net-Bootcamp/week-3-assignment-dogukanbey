6412c9d8973d4ba7a09aaab5c92c3ab7b5bcbaa1
dogukanbey <dogukandiragan@hotmail.com>
2022-06-11 12:19:39 +0300

    2ndCommit

---
+++ NLayerWeek3.API/Controllers/ProductFeatureController.cs
@@ -0,0 +1,85 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayerWeek3.Data.Models;
+using NLayerWeek3.Services.Dtos;
+using NLayerWeek3.Services.Services;
+
+namespace NLayerWeek3.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductFeatureController : ControllerBase
+    {
+
+        private readonly IMapper _mapper;
+        private readonly IProductFeatureService _service;
+        public ProductFeatureController(IMapper mapper, IProductFeatureService productFeatureService)
+        {
+
+            _mapper = mapper;
+            _service = productFeatureService;
+        }
+
+
+
+        /// GET api/products
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var categories = await _service.GetAllAsync();
+            var categoriesDtos = _mapper.Map<List<CategoryDto>>(categories.ToList());
+            return Ok(categoriesDtos);
+        }
+
+
+
+
+        // GET /api/products/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+
+
+            var productFeature = await _service.GetByIdAsync(id);
+            var productFeatureDtos = _mapper.Map<ProductFeatureDto>(productFeature);
+            return Ok(productFeatureDtos);
+        }
+
+
+
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductFeatureDto productFeatureDto)
+        {
+            await _service.UpdateAsync(_mapper.Map<ProductFeature>(productFeatureDto));
+
+            return Ok(productFeatureDto);
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductFeatureDto productFeatureDto)
+        {
+            var productFeature = await _service.AddAsync(_mapper.Map<ProductFeature>(productFeatureDto));
+            var productFeaturesDto = _mapper.Map<ProductFeatureDto>(productFeature);
+            return Ok(productFeaturesDto);
+        }
+
+
+
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var productFeature = await _service.GetByIdAsync(id);
+
+            await _service.RemoveAsync(productFeature);
+
+            return Ok();
+        }
+
+    }
+}
---
+++ NLayerWeek3.Data/Repositories/IProductFeatureRepository.cs
@@ -0,0 +1,13 @@
+﻿using NLayerWeek3.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerWeek3.Data.Repositories
+{
+    public interface IProductFeatureRepository : IGenericRepository<ProductFeature>
+    {
+    }
+}
